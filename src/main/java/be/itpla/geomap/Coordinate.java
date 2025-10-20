1f4c02941990669ad8c37ad2a739aa59af580fb6
Alessandro Pluchino <aleplu@itpla.be>
2025-10-20 20:19:53 +0200

    completed class definition

--- src/main/java/be/itpla/geomap/Coordinate.java
+++ src/main/java/be/itpla/geomap/Coordinate.java
@@ -10,13 +10,13 @@ import java.util.Comparator;
  *
  * @author Alessandro
  */
-public class Coordinate implements Comparator<Coordinate> {
+public class Coordinate {
     
     private int deg;
-    private int min;
+    private float min;
     private float sec;
 
-    public Coordinate(int deg, int min, float sec) {
+    public Coordinate(int deg, float min, float sec) {
         this.deg = deg;
         this.min = min;
         this.sec = sec;
@@ -26,11 +26,4 @@ public class Coordinate implements Comparator<Coordinate> {
         return deg+(min/60)+(sec/3600);
     }
 
-    @Override
-    public int compare(Coordinate o1, Coordinate o2) {
-        if (o1.toDD() > o2.toDD())
-            return 1;
-        else
-            return -1;
-    }
 }
