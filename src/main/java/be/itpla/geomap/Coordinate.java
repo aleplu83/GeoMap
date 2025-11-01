d8d694397db2342e157239f99ddc8c4b7efab704
Alessandro Pluchino <aleplu@itpla.be>
2025-11-01 11:39:25 +0100

    remove second Coordinate constructor

--- src/main/java/be/itpla/geomap/Coordinate.java
+++ src/main/java/be/itpla/geomap/Coordinate.java
@@ -27,14 +27,6 @@ public class Coordinate {
         
     }
     
-    public Coordinate(String DMS) {
-        // for passing Coordinates like e.g. "0501020"
-        int deg=Integer.parseInt(DMS.substring(0, 3)); // extract "050"
-        float min=Float.parseFloat(DMS.substring(4, 5)); // extract "10"
-        float sec=Float.parseFloat(DMS.substring(5));  // extract "20"
-        dd=deg+(min/60)+(sec/3600); // Convert to Decimal Degrees
-    }
-    
     public float getDD() {
         return dd;
     }
